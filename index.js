afc4759dc888744a1ab1472d520958180a8723bb
itwithlyam <64089164+itwithlyam@users.noreply.github.com>
2021-04-22 19:31:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+const Discord = require('discord.js');
+const Canvas = require('canvas')
+const {prefix, token} = require('./config.json');
+const client = new Discord.Client();
+const fs = require("fs");
+client.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+  console.log(`Command loaded: ${command.name}`);
+}
+
+
+
+client.once('ready', () => {
+    console.log('bot is onilne');
+    client.user.setActivity(`In ${client.guilds.cache.size} Servers`, { type: 'PLAYING' });
+});
+
+client.on('message', async message => {
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const commandName = args.shift().toLowerCase();
+    if (!client.commands.has(commandName)) return;
+    const command = client.commands.get(commandName);
+
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
+
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+    }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+    
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+    
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    }
+    try {
+        command.execute(message, args, Discord, client);
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command.');
+    }
+})
+
+
+client.login(token);
